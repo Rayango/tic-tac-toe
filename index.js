56ae5b87a1ca4374c6837e10d772c6755288238e
Ryan Ngo <rayango92@gmail.com>
2018-02-26 10:46:03 -0800

    Complete tic tac toe

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const readlineSync = require('readline-sync');
 
 let board = Array(9).fill(false, 0);
 let gameState = {
-  winnerFound: false,
   turnNumber: 0,
   player: Math.floor(Math.random() * 2)
 };
@@ -36,14 +35,14 @@ const executeMove = (position, player) => {
 
 const checkForWinner = (mostRecentMove, player) => {
   let piece = player ? 'o' : 'x';
-  if (checkHorziontal(mostRecentMove, piece) || checkVertical(mostRecentMove, piece) || 
+  if (checkHorizontal(mostRecentMove, piece) || checkVertical(mostRecentMove, piece) || 
       checkDiagonalLtB(mostRecentMove,piece) || checkDiagonalRtB(mostRecentMove, piece)) {
     return true;
   }
   return false;
 };
 
-const checkHorziontal = (position, piece) => {
+const checkHorizontal = (position, piece) => {
   if (position <= 2 && board[0] === piece && board[1] === piece && board[2] === piece) {
     return true;
   } else if (position <= 5 && board[3] === piece && board[4] === piece && board[5] === piece) {
@@ -56,14 +55,14 @@ const checkHorziontal = (position, piece) => {
 
 const checkVertical = (position, piece) => {
   let columnTop = position;
-  while (position > 2) {
+  while (columnTop > 2) {
     columnTop -= 3;
   }
-  if (position === 0 && board[0] === piece && board[3] === piece && board[6] === piece) {
+  if (columnTop === 0 && board[0] === piece && board[3] === piece && board[6] === piece) {
     return true;
-  } else if (position === 1 && board[1] === piece && board[4] === piece && board[7] === piece) {
+  } else if (columnTop === 1 && board[1] === piece && board[4] === piece && board[7] === piece) {
     return true;
-  } else if (position === 2 && board[2] === piece && board[5] === piece && board[8] === piece) {
+  } else if (columnTop === 2 && board[2] === piece && board[5] === piece && board[8] === piece) {
     return true;
   }
   return false;
@@ -77,10 +76,21 @@ const checkDiagonalRtB = (position, piece) => {
   return board[2] === piece && board[4] === piece && board[6] === piece;
 };
 
+while (true) {
+  if (gameState.turnNumber === 9) {
+    console.log(`
+      Cat's Game!
 
-while (!gameState.winnerFound) {
+      ${board[0] ? board[0] : 0}  ${board[1] ? board[1] : 1}  ${board[2] ? board[2] : 2}
+      ${board[3] ? board[3] : 3}  ${board[4] ? board[4] : 4}  ${board[5] ? board[5] : 5}
+      ${board[6] ? board[6] : 6}  ${board[7] ? board[7] : 7}  ${board[8] ? board[8] : 8}
+    `);
+    break;
+  }
+
   let position = promptForMove(gameState.turnNumber);
   while (board[position]) {
+    console.log('Invalid move!  Try again.');
     position = promptForMove(gameState.turnNumber);
   }
   executeMove(position, gameState.player);
@@ -96,61 +106,3 @@ while (!gameState.winnerFound) {
   gameState.player = !gameState.player;
   gameState.turnNumber++;
 }  
-
- 
-
-
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// let gameState = true;
-// let currentTurn = Math.floor(Math.random() * 2);
-// let pieces = {
-//   0: 'o',
-//   1: 'x'
-// };
-// let board = [
-//   [false, false, false], 
-//   [false, false, false], 
-//   [false, false, false]
-// ];
-
-// const makeMove = (position) => {
-//   switch(position) {
-//     case '1':
-//       board[0, 0] = pieces[currentTurn];
-//       break;
-//   }
-//   currentTurn = !currentTurn
-//   return board;
-// };
-
-// const getMove = () => {
-//   rl.question(`
-//     Welcome to tic tac toe!  To make your move, please enter the number at which you want to make your move.
-//     Player ${currentTurn === 0 ? 1 : 2} goes first!
-
-//     [${board[0][0] ? (board[0][0] === 1 ? o : x) : '1'}, 2, 3]
-//     [4, 5, 6]
-//     [7, 8, 9]\n\n`, (answer) => {
-//     makeMove(answer);
-//     if (gameState) {
-//       getMove();
-//     } 
-//     rl.close();
-//   }); 
-// };
-
-// while (gameState) {
-//   rl.question(`
-//     Player ${currentTurn === 0 ? 1 : 2}'s turn!
-
-//     [${board[0][0] ? board[0][0] : '1'}, 2, 3]
-//     [4, 5, 6]
-//     [7, 8, 9]\n\n`, (answer) => {
-//     makeMove(answer);
-//   });
-// };
